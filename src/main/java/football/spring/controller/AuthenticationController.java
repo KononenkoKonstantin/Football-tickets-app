82555bb3a8f5a5b06c0ab7a94b629f77e8176cdb
Kostiantyn <konkos2505@gmail.com>
2021-07-01 18:51:23 +0300

    add logger

--- src/main/java/football/spring/controller/AuthenticationController.java
+++ src/main/java/football/spring/controller/AuthenticationController.java
@@ -6,6 +6,8 @@ import football.spring.model.User;
 import football.spring.service.AuthenticationService;
 import football.spring.service.mapper.UserMapper;
 import javax.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authService;
     private final UserMapper userMapper;
+    private static final Logger logger = LogManager.getLogger(AuthenticationController.class);
 
     public AuthenticationController(AuthenticationService authService, UserMapper userMapper) {
         this.authService = authService;
@@ -23,6 +26,7 @@ public class AuthenticationController {
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody @Valid UserRequestDto requestDto) {
         User user = authService.register(requestDto.getEmail(), requestDto.getPassword());
+        logger.info("user with email={} registered", requestDto.getEmail());
         return userMapper.mapToDto(user);
     }
 }
